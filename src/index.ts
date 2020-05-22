cfc9e1f1dcc524e28414380c15e865e97eae897b
Scott Doxey <neogeek0101@gmail.com>
2020-05-22 02:39:39 -0400

    Removed client logic.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { EphemeralDataStore } from './server/datastore';
 
-export { WebSocketGameLobbyClient } from './client';
-
 export { WebSocketGameLobbyServer } from './server';
 
 export { DataStore, Game, Player, Spectator, Turn } from './types';
