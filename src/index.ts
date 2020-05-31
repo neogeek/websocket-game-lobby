936d12116e519d0453b3c4a7064eb3caf3432fff
Scott Doxey <neogeek0101@gmail.com>
2020-05-31 07:38:55 -0400

    Added missing types to export.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,12 @@ export { EphemeralDataStore, PostgresDataStore } from './datastore';
 
 export { WebSocketGameLobbyServer } from './server';
 
-export { DataStore, Game, Player, Spectator, Turn } from './types';
+export {
+    DataStore,
+    DataStoreEvents,
+    Game,
+    Player,
+    ServerEvents,
+    Spectator,
+    Turn
+} from './types';
